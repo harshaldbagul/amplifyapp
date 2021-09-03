242b2c9861e253f3487e4c487b5ec7f0647fc5f0
Harshal Bagul <HarshalB@talentica-all.com>
2021-09-03 14:46:31 +0530

    added upload image for notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,15 @@ import {
   CognitoUserInterface,
   AuthState,
 } from '@aws-amplify/ui-components';
-import Amplify from 'aws-amplify';
+import Amplify, { API, Storage } from 'aws-amplify';
+import { listNotes } from '../graphql/queries';
 import config from '../aws-exports';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import {
+  createNote as createNoteMutation,
+  deleteNote as deleteNoteMutation,
+} from '../graphql/mutations';
 Amplify.configure(config);
 
 @Component({
@@ -14,18 +21,80 @@ Amplify.configure(config);
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'amplify-angular-auth';
   user: CognitoUserInterface | undefined;
   authState: AuthState;
 
-  constructor(private ref: ChangeDetectorRef) {}
+  formData: FormGroup;
+  notes = [];
+
+  constructor(private ref: ChangeDetectorRef, private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.setAuthState();
+    this.resetFormData();
+    this.fetchNotes();
+  }
+
+  setAuthState() {
     onAuthUIStateChange((authState, authData) => {
       this.authState = authState;
       this.user = authData as CognitoUserInterface;
       this.ref.detectChanges();
     });
+  }
+
+  resetFormData() {
+    this.formData = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      image: [null],
+    });
+  }
+
+  async uploadImage(event) {
+    if (!event.target.files[0]) return;
+    const file = event.target.files[0];
+    this.formData.patchValue({ image: file.name });
+    await Storage.put(file.name, file);
+  }
+
+  async fetchNotes() {
+    const res: any = await API.graphql({ query: listNotes });
+    const notes = res.data.listNotes.items;
+
+    await Promise.all(
+      notes.map(async (note) => {
+        if (note.image) {
+          const image = await Storage.get(note.image);
+          note.image = image;
+        }
+        return note;
+      })
+    );
+
+    this.notes = notes;
+  }
+
+  async createNote() {
+    await API.graphql({
+      query: createNoteMutation,
+      variables: { input: this.formData.value },
+    });
+    if (this.formData.value.image) {
+      const image = await Storage.get(this.formData.value);
+      this.formData.patchValue({ image });
+    }
+    this.notes = [...this.notes, this.formData.value];
+    this.resetFormData();
+  }
+
+  async deleteNote({ id }) {
+    const newNotesArray = this.notes.filter((note) => note.id !== id);
+    await API.graphql({
+      query: deleteNoteMutation,
+      variables: { input: { id } },
+    });
+    this.notes = newNotesArray;
   }
 
   ngOnDestroy() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AmplifyUIAngularModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AmplifyUIAngularModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
